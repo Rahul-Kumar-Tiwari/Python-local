7a874796646ef1b4006e4fdebb4d9ed90f41bc4c
Rahul Kumar Tiwari <31995292+Rahul-Kumar-Tiwari@users.noreply.github.com>
2018-10-24 14:10:43 +0530

    updated input.py
    comments are added

--- input.py
+++ input.py
@@ -1,2 +1,3 @@
-name=input("Whats your name :")
+name=input("Whats your name :")     #by default as a string input
+roll=int(input("Whats your roll_no :"))   #for int input
 print("Hey",name,"Welcome to pythone")
